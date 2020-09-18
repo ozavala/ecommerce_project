537220a522afbed9b401943efc559b06eef7c0dd
Oswaldo Zavala <oszavala@gmail.com>
2020-09-18 16:25:09 -0500

    2-2 implemented cart detail functionality

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>',
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>',
          views.productPage, name='product_detail'),
-    path('cart/', views.cart, name='cart'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart', views.cart_detail, name='cart_detail'),
 ]
