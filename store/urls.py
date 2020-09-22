87d1603e6dac79b0c5a3be268574c5552a72dd47
Oswaldo Zavala <oszavala@gmail.com>
2020-09-22 12:35:36 -0500

    8-1 user product history view

--- store/urls.py
+++ store/urls.py
@@ -16,4 +16,8 @@ urlpatterns = [
     path('account/create/', views.signupView, name='signup'),
     path('account/signin/', views.signinView, name='signin'),
     path('account/signout/', views.signoutView, name='signout'),
+    path('order_history/', views.orderHistory, name='order_history'),
+    #path('order/<int:order_id>', views.viewOrder, name='order_detail'),
+    #path('search/', views.search, name='search'),
+    #path('contact/', views.contact, name='contact')
 ]
