c051951c34750e03cd57364aabc4f020d7452259
Oswaldo Zavala <oszavala@gmail.com>
2020-09-23 15:47:24 -0500

    Language translation i18n markup cart,home, navbar & product templates

--- store/views.py
+++ store/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.template.loader import get_template
 from django.core.mail import EmailMessage
+from django.utils.translation import gettext as _  # new
 
 
 def home(request, category_slug=None):
