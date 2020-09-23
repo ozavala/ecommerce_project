c467bcb5cf6da7ceca837e40b7539d0ef788adde
Oswaldo Zavala <oszavala@gmail.com>
2020-09-23 10:27:11 -0500

    11 implemented product reviews

--- store/models.py
+++ store/models.py
@@ -118,3 +118,12 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return self.product
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.content
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Category, Product, Cart, CartItem, Order, OrderItem
+from .models import Category, Product, Cart, CartItem, Order, OrderItem, Review
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 import stripe
@@ -31,7 +31,14 @@ def productPage(request, category_slug, product_slug):
             category__slug=category_slug, slug=product_slug)
     except Exception as e:
         raise e
-    return render(request, 'product.html', {'product': product})
+    if request.method == 'POST' and request.user.is_authenticated and request.POST['content'].strip() != '':
+        Review.objects.create(product=product,
+                              user=request.user,
+                              content=request.POST['content'])
+
+    reviews = Review.objects.filter(product=product)
+
+    return render(request, 'product.html', {'product': product, 'reviews': reviews})
 
 
 def _cart_id(request):
